95965b75baed42c28fd494eba700571e02f933fc
Tang Likang <tanglikang@163.com>
2014-11-07 14:30:46 +0800

    Update db.config.php

--- db.config.php
+++ db.config.php
@@ -1,14 +1,5 @@
 <?php
-/*
-	define('HOST', '121.40.157.36');
-	define('DB', 'of_backstage');
-	define('USER', 'root');
-	define('PASSWORD', 'xiangtao@2014');
-	
-	define('HOST', 'localhost');
-	define('DB', 'wordpress');
-	define('USER', 'root');
-	define('PASSWORD', '');*/
+
 define('HOST', '192.168.1.101');
 define('DB', 'of_backstage');
 define('USER', 'dev3');
