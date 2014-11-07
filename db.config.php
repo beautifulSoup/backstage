3baf1601628b9c24e85d46c29a6af27aa161a9b0
Tang Likang <tanglikang@163.com>
2014-11-07 14:31:39 +0800

    Update db.config.php

--- db.config.php
+++ db.config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('HOST', '192.168.1.101');
-define('DB', 'of_backstage');
-define('USER', 'dev3');
-define('PASSWORD', 'xiangtao2014');
+	define('HOST', 'localhost');
+	define('DB', 'tasktracking');
+	define('USER', 'root');
+	define('PASSWORD', '');
 	
 	
 ?>
